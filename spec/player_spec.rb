2258c204f706fdc15d874e755a6bb861276d287f
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-09-09 15:46:10 +0100

    refactoring tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,39 +2,95 @@ require 'player'
 
 describe Player do
 
-  let(:ship) {double :ship, position: "A1"}
+  let(:ship) {double :ship, coordinates: "A1", got_hit?: true}
 
-  it "can place a ship on the board" do
-    expect(subject.place(:ship)).to eq(subject.board)
+  it "we can place on board" do
+    expect(subject.place ship).to eq(subject.board)
   end
 
-  it "can tell us when a ship is hit" do
+  it "we can fire at the right coordinates" do
     subject.place(ship)
-    expect(subject.fire("A1")).to eq("You've hit a ship!")
+    expect(subject.fire "A1").to eq "You've hit a ship!"
   end
 
-  it "can tell us when a ship is missed" do
+  it "we missed a ship when we fire" do
     subject.place(ship)
-    expect(subject.fire("B4")).to eq("You've missed a ship!")
+    expect(subject.fire "C1").to eq "You've missed a ship!"
   end
 
-  it "can report hit positions" do
-    subject.place(ship)
-    subject.fire("A1")
-    expect(subject.hits).to eq(["A1"])
-  end
 
-  it "can report missed positions" do
-    subject.place(ship)
-    subject.fire("B2")
-    expect(subject.hits).to eq([])
-  end
 
-  it "the game ends when all ships have been hit" do
-    subject.place(ship)
-    subject.fire("A1")
-    expect(subject).to be_game_over
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # it "can place a ship on the board" do
+  #   expect(subject.place(:ship)).to eq(subject.board)
+  # end
+
+   # it "can tell us when a ship is hit" do
+   #   subject.place(ship)
+   #   expect(subject.fire("A1")).to eq("You've hit a ship!")
+   # end
+
+  # it "can tell us when a ship is missed" do
+  #   subject.place(ship)
+  #   expect(subject.fire("B4")).to eq("You've missed a ship!")
+  # end
+
+  # it "can report hit positions" do
+  #   subject.place(ship)
+  #   subject.fire("A1")
+  #   expect(subject.hits).to eq(["A1"])
+  # end
+
+  # it "can report missed positions" do
+  #   subject.place(ship)
+  #   subject.fire("B2")
+  #   expect(subject.hits).to eq([])
+  # end
+
+  # it "the game ends when all ships have been hit" do
+  #   subject.place(ship)
+  #   subject.fire("A1")
+  #   expect(subject).to be_game_over
+  # end
 
   # can lose
 
